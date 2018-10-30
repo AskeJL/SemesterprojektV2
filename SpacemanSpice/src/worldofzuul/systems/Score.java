53bc8156c622109cd03e672cdaddd78d5a0a1a8a
Unknown <lomedil.np@gmail.com>
2018-10-30 13:03:15 +0100

    Added:
    System package
    Wave class
    Score class

--- SpacemanSpice/src/worldofzuul/systems/Score.java
+++ SpacemanSpice/src/worldofzuul/systems/Score.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package worldofzuul.systems;
 
-/**
- *
- * @author Nikos
- */
 public class Score {
+    //Variable the keeps the score
+    private int score;
     
+    //Final variables that keep the amount of points awarded for each fragment
+    private final int SMALL_FRAGMENT_POINTS = 5;
+    private final int MEDIUM_FRAGMENT_POINTS = 10;
+    private final int LARGE_FRAGMENT_POINTS = 15;
+    
+    //Score constructor
+    Score(){
+        
+    }
+    
+    //Method that updates Score
+    public void updateScore(){
+        
+    }
+    
+    //toString method
+    @Override
+    public String toString(){
+      String s = "";
+      return s;
+    }
+    
+    //Method to get score
+    public void getScore(){
+        
+    }
 }
--- SpacemanSpice/src/worldofzuul/systems/Wave.java
+++ SpacemanSpice/src/worldofzuul/systems/Wave.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package worldofzuul.systems;
 
-/**
- *
- * @author Nikos
- */
 public class Wave {
+    //Variable that holds the number of the current wave
+    private int numberOfWaves;
     
+    //Variables that hold the amount of fragments for each size
+    private int smallFragments;
+    private int mediumFragments;
+    private int largeFragments;
+    
+    //Constructor
+    Wave(){
+        
+    }
+    
+    //toString method
+    @Override
+    public String toString(){
+        String s = "";
+        return s;
+    }
+    
+    //Set number of waves
+    public void setNumberOfWaves(int n){
+        this.numberOfWaves = n;
+    }
+    
+    //Get number of waves
+    public int getNumberOfWaves(){
+        return this.numberOfWaves;
+    }
+    
+    //Set number of small fragments
+    public void setSmallFragments(int n){
+        this.smallFragments = n;
+    }
+    
+    //Get number of small fragments
+    public int getSmallFragments(){
+        return this.smallFragments;
+    }
+    
+    //Set number of medium fragments
+    public void setMediumFragments(int n){
+        this.mediumFragments = n;
+    }
+    
+    //Get number of medium fragments
+    public int getMediumFragments(){
+        return this.mediumFragments;
+    }
+    //Set number of large fragments
+    public void setLargeFragments(int n){
+        this.largeFragments = n;
+    }
+    
+    //Get number of large fragments
+    public int getLargeFragments(){
+        return this.largeFragments;
+    }
 }
