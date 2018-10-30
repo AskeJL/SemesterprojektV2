e6880fcf1b111e7ced283208e68fc41c5b8a103e
Unknown <s.bangslund@hotmail.dk>
2018-10-30 09:45:55 +0100

    Added WorldOfZuul

---
+++ SpacemanSpice/src/worldofzuul/CommandWord.java
@@ -0,0 +1,19 @@
+package worldofzuul;
+
+public enum CommandWord
+{
+    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?");
+    
+    private String commandString;
+    
+    CommandWord(String commandString)
+    {
+        this.commandString = commandString;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return commandString;
+    }
+}
---
+++ SpacemanSpice/src/worldofzuul/Main.java
@@ -0,0 +1,15 @@
+package worldofzuul;
+
+/**
+ *
+ * @author sbang
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Game().play();
+    }
+}
