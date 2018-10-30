59f304d4dc142333fb13b38461883faa9367b677
AskeJL <aske.lorenz@gmail.com>
2018-10-30 20:06:37 +0100

    Net location
    Made net location

--- SpacemanSpice/src/spacemanspice/locations/functional/Net.java
+++ SpacemanSpice/src/spacemanspice/locations/functional/Net.java
@@ -5,10 +5,39 @@
  */
 package spacemanspice.locations.functional;
 
+import spacemanspice.locations.Location;
+import spacemanspice.locations.Room;
+
 /**
  *
  * @author sbang
  */
-public class Net {
+public class Net extends Location {
+    
+    
+    public Net() {
+        super("This it the net location", "In this room you can control the net and catch debree");
+        
+        createLocation();
+    }
+    @Override
+    public void createLocation() {
+        
+        Room NetCorridor = new Room("Net corridor","You are now in the net corridor");
+        super.addRoom(NetCorridor);
+        
+        Room NetRoom = new Room("net Room","Here you control the space net");
+        super.addRoom(NetRoom);
+        
+        Room NetBay = new Room("Net bay","Here you can fix your net");
+        super.addRoom(NetBay);
+       
+        
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
