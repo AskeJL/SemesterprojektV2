cfe9e69f2109899ff2c545fe1d4dc57f54ecfb7b
Unknown <lomedil.np@gmail.com>
2018-10-31 17:22:37 +0100

    Added design to oxygen and personal

--- SpacemanSpice/src/spacemanspice/locations/functional/Oxygen.java
+++ SpacemanSpice/src/spacemanspice/locations/functional/Oxygen.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spacemanspice.locations.functional;
 
-/**
- *
- * @author sbang
- */
-public class Oxygen {
+import spacemanspice.locations.Exit;
+import spacemanspice.locations.ExitDirection;
+import spacemanspice.locations.Location;
+import spacemanspice.locations.Room;
+
+public class Oxygen extends Location {
     
+    public Oxygen(){
+        super("Oxygen" , "Oxygen is refueled from here");
+        
+        createLocation();
+    }
+    
+    @Override
+    public void createLocation(){
+        
+        //The three rooms in oxygen are added here
+        //Main corridor
+        Room corridorRoom = new Room("Oxygen corridor" , "Main corridor of the oxygen room");
+        super.addRoom(corridorRoom);
+        //Oxygen storage
+        Room storageRoom = new Room("Oxygen storage" , "Here the oxygen is storaged");
+        super.addRoom(storageRoom);
+        //Oxygen ????
+        Room oxygenRoom = new Room("" , "");
+        super.addRoom(oxygenRoom);
+        
+        //Here we set the exits up
+        corridorRoom.addExit(new Exit(ExitDirection.NORTH, storageRoom));
+        corridorRoom.addExit(new Exit(ExitDirection.SOUTH, oxygenRoom));
+        
+        storageRoom.addExit(new Exit(ExitDirection.SOUTH, corridorRoom));
+        oxygenRoom.addExit(new Exit(ExitDirection.NORTH, corridorRoom));
+    }
+    
+    @Override
+    public String toString(){
+            return "locations.functional.Oxygen : Name[" + this.getName() + "] Description[" + this.getDescription() + "]";
+    }
 }
