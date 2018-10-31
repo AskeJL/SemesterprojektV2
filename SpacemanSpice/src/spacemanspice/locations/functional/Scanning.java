1e0387f3448cba51fb9dd5f04d0c7844b3849d80
Unknown <o.pickardt@gmail.com>
2018-10-31 14:06:12 +0100

    Added Scanner and Mainhall02

--- SpacemanSpice/src/spacemanspice/locations/functional/Scanning.java
+++ SpacemanSpice/src/spacemanspice/locations/functional/Scanning.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spacemanspice.locations.functional;
 
-/**
- *
- * @author sbang
- */
-public class Scanning {
+import spacemanspice.locations.Exit;
+import spacemanspice.locations.ExitDirection;
+import spacemanspice.locations.Location;
+import spacemanspice.locations.Room;
+
+public class Scanning extends Location {
     
+    public Scanning(){
+        super("Scanning", "Space debris can be scanned from here.");
+        createLocation();
+    }
+    
+    @Override
+    public void createLocation(){
+        Room scanningRoom = new Room("Scanning Scanning", "Space debris can be scanned from here.");
+        super.addRoom(scanningRoom);
+        super.addEntrance(scanningRoom);
+    }
+    
+    //Dette
+    //scanningRoom.addExit(new Exit(ExitDirection.WEST, ? ))
+    //scanningRoom.addExit(new Exit(ExitDirection.SOUTH, ? ))
+    //scanningRoom.addEntrance(new Entrance(EntranceDirection.WEST, ? ))
+    //scanningRoom.addEntrance(new Entrance(EntranceDirection.SOUTH, ? ))
+    
+    //Scanning.addExit(new Exit(ExitDirection.WEST, Control))
+    //Scanning.addExit(new Exit(ExitDirection.SOUTH, Personal))
+    
+    @Override
+    public String toString(){
+        return "locations.functional.Scanning : Name [" + this.getName() + "] Description [" + this.getDescription() + "]";
+    }
 }
