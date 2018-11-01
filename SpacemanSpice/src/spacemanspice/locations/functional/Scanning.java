822d2842bf8ac7010021694c1824f3425b2fcc85
Samuel Bangslund <s.bangslund@hotmail.dk>
2018-11-01 10:19:51 +0100

    Linked all the location exits
    Locations has been linked to match the map.

--- SpacemanSpice/src/spacemanspice/locations/functional/Scanning.java
+++ SpacemanSpice/src/spacemanspice/locations/functional/Scanning.java
@@ -16,17 +16,21 @@ public class Scanning extends Location {
     public void createLocation(){
         Room scanningRoom = new Room("Scanning Scanning", "Space debris can be scanned from here.");
         super.addRoom(scanningRoom);
+        
+        Room displayRoom = new Room("Display", "The most recent scan can be viewed here.");
+        super.addRoom(displayRoom);
+        
+        Room sensorRoom = new Room("Sensor", "The sensor equipment");
+        super.addRoom(sensorRoom);
+        
+        scanningRoom.addExit(new Exit(ExitDirection.NORTH, sensorRoom));
+        scanningRoom.addExit(new Exit(ExitDirection.EAST, displayRoom));
+        
+        displayRoom.addExit(new Exit(ExitDirection.WEST, scanningRoom));
+        sensorRoom.addExit(new Exit(ExitDirection.SOUTH, scanningRoom));
+        
         super.addEntrance(scanningRoom);
     }
-    
-    //Dette
-    //scanningRoom.addExit(new Exit(ExitDirection.WEST, ? ))
-    //scanningRoom.addExit(new Exit(ExitDirection.SOUTH, ? ))
-    //scanningRoom.addEntrance(new Entrance(EntranceDirection.WEST, ? ))
-    //scanningRoom.addEntrance(new Entrance(EntranceDirection.SOUTH, ? ))
-    
-    //Scanning.addExit(new Exit(ExitDirection.WEST, Control))
-    //Scanning.addExit(new Exit(ExitDirection.SOUTH, Personal))
     
     @Override
     public String toString(){
