f00d470729ebad94c76c7754a09f26d3a43bfb10
Unknown <o.pickardt@gmail.com>
2018-11-02 11:18:17 +0100

    Added and edited comments in Locations

--- SpacemanSpice/src/spacemanspice/locations/Location.java
+++ SpacemanSpice/src/spacemanspice/locations/Location.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spacemanspice.locations;
 
 import java.util.ArrayList;
 
 /**
- *
- * @author sbang
+ * Creates the class Location in which rooms can exist.
+ 
  */
 public abstract class Location {
 
