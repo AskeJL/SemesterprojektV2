8e00330df5a32b35cf08a8123dc05706df369a96
Unknown <o.pickardt@gmail.com>
2018-11-04 19:21:06 +0100

    Created Life class under Resources

---
+++ SpacemanSpice/src/controller/resources/Life.java
@@ -0,0 +1,42 @@
+package controller.resources;
+
+/**
+ * This class is used by other systems to consider the current life of the
+ * ship in the game.
+ */
+public class Life extends Element {
+    
+    //Life er en double, da vi ikke helt har besluttet os for hvor meget
+    //de forskellige typer a debris skal skade skibet (kunne være komma tal).
+    private double life;
+    
+    /**
+     * Constructor for this class.
+     */
+    public Life() {
+        
+    }
+    
+    /**
+     * The method that increases the life value of the ship.
+     */
+    public void increaseLife() {
+        
+    }
+    /**
+     * The metod that decreases the life value of the ship.
+     */
+    public void decreaseLife() {
+        
+    }
+    
+    @Override 
+    public void update() {
+        
+    }
+    
+    public double getLife() {
+        return life;
+    }
+    
+}
