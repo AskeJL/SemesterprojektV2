2b1f7a9ecff4d7627ad8a0bcb05c296efbddfabd
Samuel Bangslund <s.bangslund@hotmail.dk>
2018-11-04 15:51:13 +0100

    Added Command, Go, Help, Interact and Quit based on Design.  #51

--- SpacemanSpice/src/controller/interactions/Command.java
+++ SpacemanSpice/src/controller/interactions/Command.java
@@ -1,13 +1,61 @@
 /*
  * Created by Samuel Bangslund, Odense SDU Software Engineering 1. semester.
  */
-
 package controller.interactions;
 
 /**
  *
  * @author Samuel Bangslund
  */
-public class Command {
+public abstract class Command {
 
+    private String name;
+    private String description;
+    private boolean hasParameter;
+
+    /**
+     * Validate that this command can be used if called.
+     *
+     * @return false if the command is not accessible and true if it is.
+     */
+    public abstract boolean validateCommand();
+
+    /**
+     * Checks if the parameter is valid based on where the player currently is.
+     *
+     * @return false if the parameter isn't valid, true if it is.
+     */
+    public abstract boolean check();
+
+    /**
+     * Run the command itself. (After checking and validating the input)
+     */
+    public abstract void run();
+
+    @Override
+    public abstract String toString();
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setHasParameter(boolean bool) {
+        this.hasParameter = bool;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean hasParameter() {
+        return this.hasParameter;
+    }
 }
--- SpacemanSpice/src/controller/interactions/commands/Quit.java
+++ SpacemanSpice/src/controller/interactions/commands/Quit.java
@@ -1,13 +1,27 @@
-/*
- * Created by Samuel Bangslund, Odense SDU Software Engineering 1. semester.
- */
-
 package controller.interactions.commands;
 
-/**
- *
- * @author Samuel Bangslund
- */
-public class Quit {
+import controller.interactions.Command;
+
+public class Quit extends Command {
+
+    @Override
+    public boolean validateCommand() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean check() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
