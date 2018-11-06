c8d45851a6cd86de295b2b4f0866a69607883e6c
Unknown <lomedil.np@gmail.com>
2018-11-06 10:06:13 +0100

    Some logic added to systems controller
    #66

--- SpacemanSpice/src/controller/systems/SystemsController.java
+++ SpacemanSpice/src/controller/systems/SystemsController.java
@@ -3,6 +3,23 @@ package controller.systems;
 
 public class SystemsController {
 
+    SystemsController(){
+        
+    }
+    
+    
+    public void initSystemsController(){
+       Score score = new Score();
+       
+    }
+    /**
+     * Destroy a small fragment and update score
+     */
+    
+    public void destroySmallFragment(){
+        
+    }
+    
     public void controlWave(){
         
     }
