81755c524d8f6644abd639e51fa84ad0e9dbdfa0
AskeJL <aske.lorenz@gmail.com>
2018-11-14 16:40:18 +0100

    Start command

---
+++ SpacemanSpice/src/domain/interactions/commands/Start.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain.interactions.commands;
+
+import domain.interactions.Command;
+import domain.systems.SystemsController;
+
+public class Start extends Command {
+
+    public Start() {
+        super("start", "Starts the waves and score", false);
+    }
+
+    @Override
+    public void checkAvailableParameters() {
+        
+    }
+
+    @Override
+    public void run() {
+        SystemsController.setPlayerReady(true);
+    }
+
+    @Override
+    public String toString() {
+        return "controller.interactions.commands.Start: name[" + super.getName() + "] description[" + super.getDescription() + "]";
+    }
+
+    @Override
+    public void helpInfo() {
+        System.out.println("The start function will start the waves and score system");
+    }
+
+}
--- SpacemanSpice/src/domain/systems/SystemsController.java
+++ SpacemanSpice/src/domain/systems/SystemsController.java
@@ -7,46 +7,50 @@ import domain.resources.ResourcesController;
  * Class controlling oxygen and wave systems.
  */
 public class SystemsController extends Controller {
-    
+
     private final static int SMALL_FRAGMENT_IDENTIFIER = 1;
     private final static int MEDIUM_FRAGMENT_IDENTIFIER = 2;
     private final static int LARGE_FRAGMENT_IDENTIFIER = 3;
 
+    private static boolean playerReady = false;
+
     private static boolean smallFragmentDestroyed = false;
     private static boolean mediumFragmentDestroyed = false;
     private static boolean largeFragmentDestroyed = false;
-    
-    public static void init() {
-        
-    }
-    
-    public static void update(){
-        
-        if(ResourcesController.getCurrentTime() >= ResourcesController.getWaveTime()){
-            if(Wave.getSmallFragments() > 0 || Wave.getMediumFragments() > 0 || Wave.getLargeFragments() > 0){
-                ResourcesController.hitLife(Wave.getSmallFragments(), Wave.getMediumFragments(), Wave.getLargeFragments());
-                System.out.println("The ship was hit by fragments!");
-                System.out.println("Check the ships life using the 'show life' command!");
-            }
-            Wave.incrementNumberOfWaves();
-            Wave.createWave();
-            ResourcesController.getInitTime();
-        }
 
-        if (smallFragmentDestroyed == true) {
-            Score.updateScore(1);
-            Wave.updateWave(1);
-            setSmallFragmentDestroyed(false);
-        }
-        if (mediumFragmentDestroyed == true) {
-            Score.updateScore(2);
-            Wave.updateWave(2);
-            setMediumFragmentDestroyed(false);
-        }
-        if (largeFragmentDestroyed == true) {
-            Score.updateScore(3);
-            Wave.updateWave(3);
-            setLargeFragmentDestroyed(false);
+    public static void init() {
+
+    }
+
+    public static void update() {
+
+        if (playerReady == true) {
+            if (ResourcesController.getCurrentTime() >= ResourcesController.getWaveTime()) {
+                if (Wave.getSmallFragments() > 0 || Wave.getMediumFragments() > 0 || Wave.getLargeFragments() > 0) {
+                    ResourcesController.hitLife(Wave.getSmallFragments(), Wave.getMediumFragments(), Wave.getLargeFragments());
+                    System.out.println("The ship was hit by fragments!");
+                    System.out.println("Check the ships life using the 'show life' command!");
+                }
+                Wave.incrementNumberOfWaves();
+                Wave.createWave();
+                ResourcesController.getInitTime();
+            }
+
+            if (smallFragmentDestroyed == true) {
+                Score.updateScore(1);
+                Wave.updateWave(1);
+                setSmallFragmentDestroyed(false);
+            }
+            if (mediumFragmentDestroyed == true) {
+                Score.updateScore(2);
+                Wave.updateWave(2);
+                setMediumFragmentDestroyed(false);
+            }
+            if (largeFragmentDestroyed == true) {
+                Score.updateScore(3);
+                Wave.updateWave(3);
+                setLargeFragmentDestroyed(false);
+            }
         }
     }
 
@@ -73,8 +77,8 @@ public class SystemsController extends Controller {
     public static void setLargeFragmentDestroyed(boolean aLargeFragmentDestroyed) {
         largeFragmentDestroyed = aLargeFragmentDestroyed;
     }
-    
-    public static int getLargeFragments(){
+
+    public static int getLargeFragments() {
         return Wave.getLargeFragments();
     }
 
@@ -85,8 +89,12 @@ public class SystemsController extends Controller {
     public static int getSmallFragments() {
         return Wave.getSmallFragments();
     }
-    
-    public static int getScore(){
+
+    public static int getScore() {
         return Score.getScore();
+    }
+    
+     public static void setPlayerReady(boolean playerReady) {
+        SystemsController.playerReady = playerReady;
     }
 }
