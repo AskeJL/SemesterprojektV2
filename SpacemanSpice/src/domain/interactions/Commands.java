5203d3b9f2b57f0aa168b9aa8e94ae180a7fec3f
Samuel Bangslund <s.bangslund@hotmail.dk>
2018-11-15 18:20:02 +0100

    Hotfix: lastCommand is not longer initialized as Continue()

--- SpacemanSpice/src/domain/interactions/Commands.java
+++ SpacemanSpice/src/domain/interactions/Commands.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Commands {
 
     private static final List<Command> COMMAND_WORDS = new ArrayList<>();
-    private static Command lastCommand = new Continue();
+    private static Command lastCommand = new Clear();
     private static String lastParameter = "";
     
     private Commands(){}
