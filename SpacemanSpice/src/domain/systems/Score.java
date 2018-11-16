18582b4baa2b82b81e9da49ee19a3c0274560df3
Samuel Bangslund <s.bangslund@hotmail.dk>
2018-11-16 16:49:50 +0100

    Added comments to domain.systems

--- SpacemanSpice/src/domain/systems/Score.java
+++ SpacemanSpice/src/domain/systems/Score.java
@@ -1,41 +1,72 @@
-
 package domain.systems;
 
 /**
- * Class that keeps track of score.
+ * This keeps track of the score that the player achieves throughout the game.
+ * The score depends on how many (and of which) fragments are destroyed.
+ *
+ * @see domain.locations.functional.Laser
+ * @see domain.locations.functional.Net
+ * @see domain.locations.functional.Control
+ * @see SystemsController
+ * @see Wave
  */
 public class Score {
-    
-    private static int score = 0;
-    
-    private final static int SMALL_FRAGMENT_POINTS = 5;
-    private final static int MEDIUM_FRAGMENT_POINTS = 10;
-    private final static int LARGE_FRAGMENT_POINTS = 15;
-    
-    private Score(){}
+
     /**
-     * Method that updates score, based on fragment type.
-     * @param fragmentIdentifier 
+     * The current score of this play-through.
      */
-    static void updateScore(int fragmentIdentifier){
-        if(fragmentIdentifier == SystemsController.getSmallFragmentIdentifier()){
+    private static int score = 0;
+
+    /**
+     * How many points a small fragment adds to the {@link #score score}.
+     */
+    private final static int SMALL_FRAGMENT_POINTS = 5;
+    /**
+     * How many points a medium fragment adds to the {@link #score score}.
+     */
+    private final static int MEDIUM_FRAGMENT_POINTS = 10;
+    /**
+     * How many points a large fragment adds to the {@link #score score}.
+     */
+    private final static int LARGE_FRAGMENT_POINTS = 15;
+
+    private Score() {
+    }
+
+    /**
+     * Update the score. The score will be updated according to the
+     * fragmentIdentifier:<br>
+     * 1 : Small fragment <br>
+     * 2 : Medium fragment <br>
+     * 3 : Large fragment <br>
+     *
+     * @param fragmentIdentifier A value between 1-3. (Depending on the
+     * identifier)
+     */
+    static void updateScore(int fragmentIdentifier) {
+        if (fragmentIdentifier == SystemsController.getSmallFragmentIdentifier()) {
             score += SMALL_FRAGMENT_POINTS;
         }
-        if(fragmentIdentifier == SystemsController.getMediumFragmentIdentifier()){
+        if (fragmentIdentifier == SystemsController.getMediumFragmentIdentifier()) {
             score += MEDIUM_FRAGMENT_POINTS;
         }
-        if(fragmentIdentifier == SystemsController.getLargeFragmentIdentifier()){
+        if (fragmentIdentifier == SystemsController.getLargeFragmentIdentifier()) {
             score += LARGE_FRAGMENT_POINTS;
         }
     }
-    
+
     @Override
-    public String toString(){
-      String s = "";
-      return s;
+    public String toString() {
+        String s = "";
+        return s;
     }
-    
-    static int getScore(){
+
+    /**
+     * Get the {@link #score score}.
+     *
+     * @return
+     */
+    static int getScore() {
         return score;
     }
 }
