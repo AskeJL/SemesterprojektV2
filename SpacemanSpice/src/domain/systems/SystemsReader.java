53b1c814992c197e3c5254826db33cf97907f6c6
Samuel Bangslund <s.bangslund@hotmail.dk>
2018-11-20 11:36:24 +0100

    Implemented interfaces #42

---
+++ SpacemanSpice/src/domain/systems/SystemsReader.java
@@ -0,0 +1,38 @@
+package domain.systems;
+
+/**
+ * An interface to the domain-layer for reading systems data. The intent of
+ * this, is for the presentation layer to display some of the computations the
+ * domain.systems package is doing.
+ *
+ * @see SystemsController
+ */
+public interface SystemsReader {
+
+    /**
+     * Read the amount of small fragments.
+     *
+     * @return Number of fragments.
+     */
+    default int readSmallFragments() {
+        return SystemsController.getSmallFragments();
+    }
+
+    /**
+     * Read the amount of medium fragments.
+     *
+     * @return Number of fragments.
+     */
+    default int readMediumFragments() {
+        return SystemsController.getMediumFragments();
+    }
+
+    /**
+     * Read the amount of large fragments.
+     *
+     * @return Number of fragments.
+     */
+    default int readLargeFragments() {
+        return SystemsController.getLargeFragments();
+    }
+}
