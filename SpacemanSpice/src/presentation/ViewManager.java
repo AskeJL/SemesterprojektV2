97e0d99489388c3ace0826b72be6bfe8bfd61b1b
Samuel Bangslund <s.bangslund@hotmail.dk>
2018-11-20 13:24:47 +0100

    Implemented presentation interfaces #46

---
+++ SpacemanSpice/src/presentation/ViewManager.java
@@ -0,0 +1,24 @@
+package presentation;
+
+/**
+ * The main controller of all the view from the presentation layer. This will
+ * control which views to update and initialize.
+ */
+class ViewManager {
+
+    /**
+     * Initialize the necessary scene.
+     *
+     * @param viewIndex The index of a view.
+     */
+    static void init(int viewIndex) {
+        
+    }
+
+    /**
+     * Update a particular view. 
+     */
+    static void update() {
+        
+    }
+}
