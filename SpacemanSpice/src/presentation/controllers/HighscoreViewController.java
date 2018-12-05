aad1a4e7cdd1bf61de244b162dea4e09560904b6
AskeJL <aske.lorenz@gmail.com>
2018-12-05 22:25:08 +0100

    Highscore window shows highscores again

--- SpacemanSpice/src/presentation/controllers/HighscoreViewController.java
+++ SpacemanSpice/src/presentation/controllers/HighscoreViewController.java
@@ -1,9 +1,13 @@
 package presentation.controllers;
 
+import data.AssetType;
+import data.Data;
+import data.read.ReadController;
 import presentation.GUIManager;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,9 +15,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
 public class HighscoreViewController extends ViewController implements Initializable {
-
+    
+    Data data = new Data();
     @FXML
-    private ListView<?> highscoreLIst;
+    private ListView<String> highscoreLIst;
     @FXML
     private Button backButton;
 
@@ -24,7 +29,10 @@ public class HighscoreViewController extends ViewController implements Initializ
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        ObservableList<String> scores = highscoreLIst.getItems();
+        for (int i = 0; i<data.requestData(AssetType.SCORE, "highscore.txt").size(); i++){
+        scores.add(data.requestData(AssetType.SCORE, "highscore.txt").get(i));
+        }
     }    
 
     @FXML
