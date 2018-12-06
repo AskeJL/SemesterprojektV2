82e2dcdb9679e59e66cf1062fbe40a23623c8395
AskeJL <aske.lorenz@gmail.com>
2018-12-06 16:20:32 +0100

    Stop playing sound when dead

--- SpacemanSpice/src/presentation/controllers/ViewController_Game.java
+++ SpacemanSpice/src/presentation/controllers/ViewController_Game.java
@@ -71,13 +71,16 @@ public class ViewController_Game extends ViewController implements Initializable
 
     @Override
     public void update() {
+        sound.startSounds();
         progressBarLife.setProgress((double) reader.readLifeValue() / 100);
         if (reader.readLifeValue() == 0) {
             guiManager.loadView(guiManager.getGameOverPath());
+            sound.stopSounds();
         }
         progressBarOxygen.setProgress((double) reader.readOxygenValue() / 100);
         if (reader.readOxygenValue() == 0) {
             guiManager.loadView(guiManager.getGameOverPath());
+            sound.stopSounds();
         }
         waveTimeLabel.setText(Long.toString(reader.readRemainingTime()));
         waveNumberValue.setText(Integer.toString(reader.readNumberOfWaves()));
