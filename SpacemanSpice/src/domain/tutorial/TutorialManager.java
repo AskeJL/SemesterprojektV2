48c37985f5b03e1e07b7dce8f276c43c49b3d3d5
Unknown <o.pickardt@gmail.com>
2018-12-14 08:59:45 +0100

    Final fix for tutorial and edit of the About file

--- SpacemanSpice/src/domain/tutorial/TutorialManager.java
+++ SpacemanSpice/src/domain/tutorial/TutorialManager.java
@@ -149,7 +149,6 @@ public class TutorialManager extends Manager implements GameUpdateable {
                     case 0:
                         println(gUIintroduction);
                         counter++;
-                        System.out.println("This is the GUI tutorial.");
 
                         break;
                     case 1:
@@ -187,7 +186,7 @@ public class TutorialManager extends Manager implements GameUpdateable {
                         }
                         break;
                     case 6:
-                        if (/*scannerCalibrated == true &&*/ scannerActivated == true) {
+                        if (scannerCalibrated == true && scannerActivated == true) {
                             println(gUIaIintro6);
                             counter++;
                         }
@@ -286,7 +285,6 @@ public class TutorialManager extends Manager implements GameUpdateable {
                 case 0:
                     println(introduction);
                     counter++;
-                    System.out.println("This is not the GUI tutorial.");
 
                     break;
                 case 1:
